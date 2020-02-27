40193cc636557cfa103b355457b02b560ce8dc72
gardenk11181 <55073876+gardenk11181@users.noreply.github.com>
2020-02-27 14:56:09 +0900

    CardView added

--- app/src/main/java/com/gardenlab/samplelayout/MainActivity.java
+++ app/src/main/java/com/gardenlab/samplelayout/MainActivity.java
@@ -3,12 +3,28 @@ package com.gardenlab.samplelayout;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Layout1 layout1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        layout1 = findViewById(R.id.layout1);
+
+        layout1.setImage(R.drawable.ic_launcher_foreground);
+        layout1.setName("김정원");
+        layout1.setMobile("010-1010-1010");
+
+        Button btn = findViewById(R.id.btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                layout1.setImage(R.drawable.ic_launcher_background);
+            }
+        });
     }
 }
